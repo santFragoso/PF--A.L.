ee6d032f92fbb53d5b183caa74268d9ef2dae922
santFragoso <66925466+santFragoso@users.noreply.github.com>
2021-01-31 20:00:50 -0600

    Update conjuntoGenerador.py

--- conjuntoGenerador.py
+++ conjuntoGenerador.py
@@ -1,31 +1,47 @@
-##código para el conjunto generador
-
-
-
 def conjGenTipo1(matrizDeRelaciones):
   matrizRREF = matrizDeRelaciones.rref()
-  print(matrizRREF)  
+    
   m,n = matrizDeRelaciones.dimensions()
   columnasPivote = matrizRREF.pivots() 
+  
   numColumnasPivote = len(columnasPivote)
-  print(numColumnasPivote)  
+    
   numConjuntoGen = n - numColumnasPivote
   cardinalidadVectores = n 
   conjuntoVectoresGen = Matrix(QQ, numConjuntoGen, cardinalidadVectores) ##las filas son los vectores
   
-  for i in range(0, numConjuntoGen):
+  listaConjuntoGenerador = []
+   
+  restar = 0  
+  for i in range(0, n):
     ##copiamos la columna en negativo (numColumnasPivote + i) del rref en la fila que le corresponde
+    filaTemporal = [0]*n
+    y=0
+    for z in columnasPivote:
+        if (i == z):
+            y = 1
+            
+    if (y == 1):
+        restar = restar+1
+        continue
+        
+    
     for j in range(0,m):
-      conjuntoVectoresGen[i, j] = -( matrizRREF[j, numColumnasPivote + i] )
+      filaTemporal[j] = -( matrizRREF[j,i] )
     ##Antes de cambiar de fila(vector) agregamos los 0 pertinentes  
     for k in range(0, n - numColumnasPivote):
-      conjuntoVectoresGen[i,m + k] = 0
-      print("hola")  
-    conjuntoVectoresGen[i, m + i] = 1 ##Agregamos el 1 donde corresponde
+      filaTemporal[numColumnasPivote + k] = 0
+    
+    
+    
+    filaTemporal[numColumnasPivote + i - restar ] = 1 ##Agregamos el 1 donde corresponde
+    listaConjuntoGenerador.append(filaTemporal)
+    
+    
   
-  listaConjuntoGenerador = []
-  for l in range(0, numConjuntoGen):
-    listaConjuntoGenerador.append(conjuntoVectoresGen[l])
+
+    
+  
   return listaConjuntoGenerador
 
 def conjGenTipo2():
@@ -53,8 +69,6 @@ def conjuntoGenerador(matrizDeRelaciones, tipo, dimMConjGenMatriz, dimNConjGenMa
 
 
 ##Datos para ejecutar ejemplos:
-matrizDeRelaciones = Matrix([[1,3,3,0],[6,12,9,8]])
+matrizDeRelaciones = Matrix([[1,2,0,3],[0,0,1,-1],[0,0,2,-2],[0,0,2,-2]])
 tipo = 1 
 conjuntoGenerador(matrizDeRelaciones, tipo, 0, 0)
-
-               
