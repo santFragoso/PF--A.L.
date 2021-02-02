db657e7b9ed0f054ab27397da8e9f016b96ae10e
AValRS <77701887+AValRS@users.noreply.github.com>
2021-02-01 18:39:53 -0600

    Update espacioGenerado.py

--- espacioGenerado.py
+++ espacioGenerado.py
@@ -1,47 +1,42 @@
-##código para el espacio generado
-
-def espacioGenerado(W):
-   
-    m = len(W[0])
-    n = len(W)
-        
-    A = matrix(m,n+1)
-    for i in range(0,m):
-        for j in range(0,n):
-            A[j,i] = W[i][j]
-    return A
-
+P.<a,b,c,d>=QQ[]
 v1 = vector([1,-1,2,1]); v2 = vector([-1,1,1,2]); v3 = vector([0,0,3,3]); v4 = vector([2,-2,1,-1])
-W = (v1,v2,v3,v4)
-print(espacioGenerado(W))
-espacioGenerado(W)
-C=[]
-x1 = var('x1'); x2 = var('x2'); x3 = var('x3'); d = var('x4')
-var('a b c d')
 VectorGen = ([a,b,c,d])
-Variables = ([x1,x2,x3,x4])
-print(VectorGen)
+nv1 =[]; nv2 =[]; nv3 =[]; nv4 =[]
+W = (v1,v2,v3,v4)
+print(W)
+j=0
+for i in range (0,4):
+    nv1.append(W[i][j])
+nv1.append(a)
+j=j+1
+for i in range (0,4):
+    nv2.append(W[i][j])
+nv2.append(b)
+j=j+1
+for i in range (0,4):
+    nv3.append(W[i][j])
+nv3.append(c)
+j=j+1
+for i in range (0,4):
+    nv4.append(W[i][j])
+nv4.append(d)
+
+print(nv1)
 print(" ")
-for i in range (0,len(W)):
-    C.append(Variables[i]*W[i])
-print(C)
+print(nv2)
+print(" ")
+print(nv3)
+print(" ")
+print(nv4)
 print(" ")
 
-Eq=[]
-for i in range(0, len(W)):
-    valor=0
-    for j in range (0, len(W)):
-        valor = valor + C[j][i]
-    Eq.append(valor)
+A = matrix(P,4,5, [nv1, nv2, nv3, nv4])
+print(A)
+print(" ")
+
+AER = A.echelon_form('row_reduction')
+print(AER)
 
 print(" ")
-print(Eq)
-
-EPR=[]
-for i in range (0,len(W)):
-    EPR.append(Eq[i]==VectorGen[i])
-    
-print(" ")
-print(EPR)
-print(" ")
-solve(EPR,Variables)
+print(AER[2][4])
+print(AER[3][4])
